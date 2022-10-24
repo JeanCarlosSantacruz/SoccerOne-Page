ac6cfb21aef50894e82f10feacf5bbe48b5313ea
Gualguanosky <gualguan93@hotmail.com>
2022-10-24 12:59:06 -0500

    1111
    11

--- src/components/QuienesSomos.js
+++ src/components/QuienesSomos.js
@@ -6,7 +6,10 @@ function QuienesSomos(){
     <div>
       <Navbar/>
       <h1 className='titulo1'>¿Quienes somos?</h1>
-      <p className='parrafoquienesomos'>Especialistas en crear contenidos. Investigadores de los lugares más reconditos de Google Amantes de la gente que escribe guion sin tilde. Rápidos hasta donde la calidad no se deteriora. Eficaces en llevar a cabo un plan, tu plan. Y más allá de todo eso, tu equipo </p>
+      <div className='guiaparrafo'>
+      <p className='parrafo1'>Especialistas en crear contenidos. Investigadores de los lugares más reconditos de Google Amantes de la gente que escribe guion sin tilde. Rápidos hasta donde la calidad no se deteriora. Eficaces en llevar a cabo un plan, tu plan. Y más allá de todo eso, tu equipo </p>
+      </div>
+      
       <Cards/>
       
       
