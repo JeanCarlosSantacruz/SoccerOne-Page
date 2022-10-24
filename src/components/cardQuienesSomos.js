4ee6a4707e0588945b67de7c3ce8c79073f3f5f8
Gualguanosky <gualguan93@hotmail.com>
2022-10-24 14:22:44 -0500

    2
    11

--- src/components/cardQuienesSomos.js
+++ src/components/cardQuienesSomos.js
@@ -12,6 +12,7 @@ function Card({title, imageSource , dat}) {
         <h4 className='card-title1'>{title}</h4>
         <p className='card-text text-secondary1'>{dat}</p>
         <a className="ligasLinks1" href='/Ligas'>Mas informacion</a> 
+        <i class="bi bi-facebook"></i>
       </div>
     </div>
   )
