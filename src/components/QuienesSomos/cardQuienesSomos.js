653ec14edc79d1932c818a696c63630c92b872de
Jean Carlos Santacruz <jeancarlos@javerianacali.edu.co>
2022-10-24 21:27:56 -0500

    Organizacion de Quines somos (completa 99%)

---
+++ src/components/QuienesSomos/cardQuienesSomos.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './QuienesSomos.css'
+
+
+function Card({title, imageSource, face , ig , git ,dat}) {
+  return (
+
+    <div className='cardprincipal1'>
+
+      <img src={imageSource} alt='' className='Timg1'></img>
+      <div className='card-body1'>
+        <h4 className='card-title1'>{title}</h4>
+        <p className='card-text text-secondary1'>{dat}</p>
+        <div className='icons'>
+          <a   href={face}  className='socialMedia'>
+            <i class="fa-brands fa-facebook-f" ></i>
+          </a>
+          <a href={ig} className='socialMedia'>
+            <i class="fa-brands fa-instagram"></i>
+          </a>
+          <a href={git} className='socialMedia'>
+            <i class="fa-brands fa-github"></i>
+          </a>
+        </div>
+        <br></br>
+        <a className="ligasLinks1" href='/Ligas'>Mas informacion</a> 
+      </div>
+    </div>
+  )
+}
+
+export default Card
